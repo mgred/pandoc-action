43deb0b8e7d95339a0a8039d1f25dba243f0a17b
Marc Redemske <marc.redemske@gmail.com>
2019-11-03 16:58:51 +0100

    more debugging

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 const actions = require("@actions/exec");
 
 async function run() {
-  await actions.exec("sh", ["install.sh"]);
+  const options = {};
+  let output;
+  let error;
+  options.listeners = {
+    stdout: (data: Buffer) => {
+      output += data.toString();
+    },
+    stderr: (data: Buffer) => {
+      error += data.toString();
+    }
+  };
+  await actions.exec("ls", ["-l"], options);
+  await actions.exec("sh", ["install.sh"], options);
 }
 
-run();
+run().finally(() => console.log(output, error));
